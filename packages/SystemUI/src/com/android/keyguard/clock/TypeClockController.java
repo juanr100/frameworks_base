ec22be3f1f3d3548e165327fa8f8e2a8b4d4b63a
Juan <jramos69@gmail.com>
2020-10-12 01:28:23 -0400

    TypeClockController: Make it compile with new plugin API
    Change-Id: Ia8e3eb7535b60a776d9ecbf7372939ab701260d9

--- packages/SystemUI/src/com/android/keyguard/clock/TypeClockController.java
+++ packages/SystemUI/src/com/android/keyguard/clock/TypeClockController.java
@@ -50,6 +50,11 @@ public class TypeClockController implements ClockPlugin {
      * Extracts accent color from wallpaper.
      */
     private final SysuiColorExtractor mColorExtractor;
+    
+    /**
+     * Computes preferred position of clock.
+     */
+    private final SmallClockPosition mClockPosition;
 
     /**
      * Renders preview from clock view.
@@ -84,6 +89,7 @@ public class TypeClockController implements ClockPlugin {
         mResources = res;
         mLayoutInflater = inflater;
         mColorExtractor = colorExtractor;
+        mClockPosition = new SmallClockPosition(res);
     }
 
     private void createViews() {
@@ -131,7 +137,7 @@ public class TypeClockController implements ClockPlugin {
         setDarkAmount(1f);
         setTextColor(Color.WHITE);
         ColorExtractor.GradientColors colors = mColorExtractor.getColors(
-                WallpaperManager.FLAG_LOCK, true);
+                WallpaperManager.FLAG_LOCK);
         setColorPalette(colors.supportsDarkText(), colors.getColorPalette());
         onTimeTick();
 
@@ -154,6 +160,11 @@ public class TypeClockController implements ClockPlugin {
         return mView;
     }
 
+    @Override
+    public int getPreferredY(int totalHeight) {
+        return mClockPosition.getPreferredY();
+    }
+    
     @Override
     public void setStyle(Style style) {}
 
@@ -181,6 +192,7 @@ public class TypeClockController implements ClockPlugin {
 
     @Override
     public void setDarkAmount(float darkAmount) {
+        mClockPosition.setDarkAmount(darkAmount);
         if (mDarkController != null) {
             mDarkController.setDarkAmount(darkAmount);
         }
